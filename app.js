acc5b42eb9c3c797b3e6c5fd9c351c01d035faa8
Maxime Tenon <maxime.tenon@gmail.com>
2024-09-02 21:35:13 +0200

    added token middleware

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// function pour verifier la validitée du token
+const validateToken = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ token: req.headers.authorization });
+        if (user === null || user === undefined) {
+            return res.json({ result: false, error: "Token invalide" });
+        }
+        req.body._id = user._id;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(401)
+        res.json({ result: false, error: "Internal server error" });
+    }
+};
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
